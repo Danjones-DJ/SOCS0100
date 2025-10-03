5127b774c86767bbf4b97680fa829895d5a03b70
Daniel Jones <danieljones@Mac.lan>
2025-10-03 15:26:54 +0100

    Working Amazon auto searcher

--- Week 8/Selenium Practice.R
+++ Week 8/Selenium Practice.R
@@ -1,38 +1,99 @@
-
 library(RSelenium); library(rvest)
-driver <- rsDriver(
-  browser = "firefox",
-  geckover = "latest",        # let it fetch a recent geckodriver
-  phantomver = NULL,          # CRITICAL: do not check/download PhantomJS
-  check = FALSE,              # skip extra version checks that can trigger downloads
-  verbose = FALSE,
+
+
+# Load Driver -------------------------------------------------------------
+
+driver = rsDriver(
+  browser = "firefox", # Choose what browser to use
+  geckover = "latest", # GeckoDriver is how we bridge from Selenium to Firefox, do other browsers have their own?
+  phantomver = NULL, # Phantomver is a depracated system, turn it off
+  check = FALSE, # Don't check is the browser is where we say, assume true
+  verbose = FALSE, # Don't spam us with logs pls
   extraCapabilities = list(
     `moz:firefoxOptions` = list(
-      binary = "/Applications/Firefox.app/Contents/MacOS/firefox"
-      # , args = list("-headless")  # optional
+      binary = "/Applications/Firefox.app/Contents/MacOS/firefox" # Where are youuuuu fire fox
     )
   )
 )
 
-remDr <- driver[["client"]]
-remDr$navigate("https://github.com/Danjones-DJ/SOCS0100")
-# remDr$open("https://github.com/Danjones-DJ/SOCS0100")
+remDr = driver[["client"]] # create remDr to remotely drive around the web!
+
+# Choose destination! -----------------------------------------------------
+
+remDr$navigate("https://amazon.co.uk") # Open Amazon on my firefox
 
 
-remDr$getTitle()
-remDr$getCurrentUrl()
+# Find pop-up -------------------------------------------------------------
+Sys.sleep(1)
+tryCatch({
+  # Try to find the popover
+  popover = remDr$findElement(using = "css selector", value = ".a-popover-wrapper")
+  
+  # If we got here, it exists! Now click close
+  close = remDr$findElement(using = "css selector", value = ".a-icon-close")
+  close$clickElement()
+  cat("✓ Popover closed\n")
+  
+}, error = function(e) {
+  # Element doesn't exist, that's fine
+  cat("✓ No popover to close\n")
+})
 
-# I want to open week 7 folder
-# Find the "Week 7" link and click it
-week7_button <- remDr$findElement(using = "link text", value = "Week 7")
-week7_button$clickElement()
+# Next wait for cookies to bake
+Sys.sleep(1)
+
+# Aha!
+detect_cookie = function(driver) {
+  tryCatch({
+    banner = driver$findElement(using="css selector", value="#cos-banner")
+    cat("cos-banner detected! \n")
+    return(banner)
+  }, error = function(e) {
+    cat("cos-banner not detected! FUCK!!!!! \n")
+    return(NULL)
+  })
+}
+banner = detect_cookie(remDr)
+
+# Accept the cookie
+accept = remDr$findElement(using="css selector", value = "span.a-declarative:nth-child(1)")
+accept$clickElement()
+print("cookies accepted")
+
+# it works haha
+
+
+# Interacting with Search -------------------------------------------------
+
+search_box = remDr$findElement(using="css selector", value="#twotabsearchtextbox")
+search_box$sendKeysToElement(list("Rubber Ducky"))
+
+# Click Search ------------------------------------------------------------
+search_button = remDr$findElement(using="css selector", value="#nav-search-submit-button")
+search_button$clickElement()
 
 
 
+# Open Sort ---------------------------------------------------------------
+
+sort_dropdown = remDr$findElement(using="css selector", value="#a-autoid-0-announce")
+sort_dropdown$clickElement()
 
 
-remDr$getActiveElement()
-remDr$close()
+# Choose Avg reviews ------------------------------------------------------
+
+sort_by_reviews = remDr$findElement(using="css selector", value="#s-result-sort-select_3")
+sort_by_reviews$clickElement()
+
+# Fucking boss la
+Sys.sleep(3)
+
+# Now only 4+ stars -------------------------------------------------------
+best_reviews_only = remDr$findElement(using="css selector", value="#filter-p_72 > span:nth-child(1)")
+best_reviews_only$clickElement()
+
+
+# STOPPPPP ----------------------------------------------------------------
 driver[["server"]]$stop()
 
 
