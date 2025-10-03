b47de55d81dd89a8eee13fe16c4042c2d4bff2a4
Daniel Jones <danieljones@eduroam-int-dhcp-97-43-59.ucl.ac.uk>
2025-10-03 11:11:56 +0100

    Done 2

--- Week 7/Lecture 7.R
+++ Week 7/Lecture 7.R
@@ -1,4 +1,4 @@
-library(tidyverse); library(rvest)
+library(tidyverse); library(rvest); library(plotly)
 
 yahoo_indices_url = "https://finance.yahoo.com/world-indices/"
 yahoo_parsed = read_html(yahoo_indices_url)
@@ -7,4 +7,32 @@ yahoo_index_table_xpath = '//*[@id="main-content-wrapper"]/section[1]/div/div/di
 
 main_table = html_element(yahoo_parsed, xpath=yahoo_index_table_xpath)
 main_table_df = html_table(main_table) %>% janitor::clean_names()
-View(main_table_df)
+# View(main_table_df)
+
+
+# clean price -------------------------------------------------------------
+main_table_clean = main_table_df %>%
+  mutate(
+    price = str_remove_all(price, ","),
+    price = str_extract(price, "^[0-9]+\\.?[0-9]*") %>% as.numeric()
+  ) %>%
+  select("name", "price", "change_percent")
+#View(main_table_clean)
+
+yahoo_data = main_table_clean 
+
+# Plotly
+
+yahoo_graph = plot_ly(
+  data = yahoo_data,
+  x = ~name,
+  y = ~change_percent, 
+  name = "Yahooooo",
+  type = "bar"
+  
+)
+
+yahoo_graph
+
+# Save the plot as an HTML file
+htmlwidgets::saveWidget(yahoo_graph, "yahoo_graph.html")
