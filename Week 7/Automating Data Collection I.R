4bbc36e852e10b5dae578871c984e01e7c3396f2
Daniel Jones <danieljones@Daniels-MacBook-Pro-2.local>
2025-10-10 12:21:52 +0100

    Added lab exercise with Plotly

--- Week 7/Automating Data Collection I.R
+++ Week 7/Automating Data Collection I.R
@@ -6,6 +6,7 @@ pacman::p_load(tidyverse, # tidyverse pkgs including purrr
                purrr, # automating 
                rvest, # parsing HTML
                janitor, # clean
+               plotly, # graphing
                robotstxt) #checking path is permitted 
 
 
@@ -70,4 +71,38 @@ get_table_from_wiki = function(url) {
 map(url_list, get_table_from_wiki, .progress=TRUE) # bosh
 
 
+# -------------------------------------------------------------------------
 
+# Scraping Yahoo Lab
+paths_allowed("https://finance.yahoo.com/world-indices/") # [1] TRUE
+
+# parse yahoo
+yahoo_url = "https://finance.yahoo.com/world-indices/"
+parsedYahoo = read_html(yahoo_url)
+
+# Get table with xpath
+indiceTable = html_element(parsedYahoo, xpath='//*[@id="main-content-wrapper"]/section[1]/div/div/div/table')
+indiceTable_df = html_table(indiceTable) %>% janitor::clean_names()
+print(indiceTable_df) # ugly price
+
+indiceTable_df_clean = indiceTable_df %>%
+  mutate(price = str_remove(price, "-.*$")) %>%
+  select(name, price, change_percent) %>%
+  mutate(change_percent = as.numeric(gsub("%", "", change_percent)))
+
+#str(indiceTable_df_clean)
+
+
+# Plotting with Plotly ----------------------------------------------------
+yahoo_data = indiceTable_df_clean %>%
+  arrange(change_percent) %>%
+  mutate(name=factor(name, levels=name))
+
+plot = plot_ly(
+  data = yahoo_data,
+  x = ~ name,
+  y = ~ change_percent,
+  type = "bar"
+)
+
+plot
